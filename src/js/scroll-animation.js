c8137c98284f2f3297cc2ca7cd9aa2762577e3c0
otaviomulato <otavio.mmulato@outlook.com.br>
2025-10-23 10:10:15 -0300

    estratura básica

---
+++ src/js/scroll-animation.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cards = document.querySelectorAll('.testimonial-card');
+
+    const observerOptions = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.1 
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
